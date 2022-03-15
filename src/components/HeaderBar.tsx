6bcdf4c9a9560405a553d62c7222f1fdb9263e01
Yujŏnja <hardboiled65@gmail.com>
2022-03-16 08:54:54 +0900

    Some styles changed.

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -7,7 +7,17 @@ const HeaderBar = () => {
     <header
       className="od-header-bar"
     >
-      <div>Orbit Developer</div>
+      <a
+        className="od-header-bar__link row items-center"
+        href="/"
+      >
+        <img
+          className="od-header-bar__link__logo"
+          src="https://raw.githubusercontent.com/orbitrc/developer.orbitrc.io/main/static/orbit-logo-dev%40512x512.png"
+          width="48"
+        />
+        Orbit Developer
+      </a>
     </header>
   );
 }
