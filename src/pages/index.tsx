debe78e6824047c18fea5130e9c3d345fe24ec8b
Aspen Schneider <hardboiled65@gmail.com>
2024-04-17 14:40:50 +0900

    Comment out test page links from index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,10 +73,18 @@ const IndexPage = () => {
           Wayland client development guide
         </Link>
         <br />
+        <Link to="https://github.com/orbitrc"
+          target="_blank"
+        >
+          Orbit Research Centre GitHub
+        </Link>
+        <br />
+        {/*
         <Link to="/page-2/">Go to page 2</Link> <br />
         <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
         <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
         <Link to="/using-dsg">Go to "Using DSG"</Link>
+        */}
       </p>
     </IndexLayout>
   );
